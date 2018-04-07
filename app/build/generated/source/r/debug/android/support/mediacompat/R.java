e11acc4bcf96a69b6e4733df47b88584fbc4e9f6
meganzhao <zhaom@carleton.edu>
2018-04-07 15:51:18 -0500

    added log in page and log out function

--- app/build/generated/source/r/debug/android/support/mediacompat/R.java
+++ app/build/generated/source/r/debug/android/support/mediacompat/R.java
@@ -86,18 +86,18 @@ public final class R {
         public static final int line1 = 0x7f08008d;
         public static final int line3 = 0x7f08008e;
         public static final int media_actions = 0x7f080093;
-        public static final int normal = 0x7f0800a2;
-        public static final int notification_background = 0x7f0800a3;
-        public static final int notification_main_column = 0x7f0800a4;
-        public static final int notification_main_column_container = 0x7f0800a5;
-        public static final int right_icon = 0x7f0800bc;
-        public static final int right_side = 0x7f0800bd;
-        public static final int status_bar_latest_event_content = 0x7f0800e7;
-        public static final int tag_transition_group = 0x7f0800ec;
-        public static final int text = 0x7f0800ed;
-        public static final int text2 = 0x7f0800ee;
-        public static final int time = 0x7f0800f5;
-        public static final int title = 0x7f0800f6;
+        public static final int normal = 0x7f0800a3;
+        public static final int notification_background = 0x7f0800a4;
+        public static final int notification_main_column = 0x7f0800a5;
+        public static final int notification_main_column_container = 0x7f0800a6;
+        public static final int right_icon = 0x7f0800bd;
+        public static final int right_side = 0x7f0800be;
+        public static final int status_bar_latest_event_content = 0x7f0800e8;
+        public static final int tag_transition_group = 0x7f0800ed;
+        public static final int text = 0x7f0800ee;
+        public static final int text2 = 0x7f0800ef;
+        public static final int time = 0x7f0800f6;
+        public static final int title = 0x7f0800f7;
     }
     public static final class integer {
         public static final int cancel_button_image_alpha = 0x7f090004;
@@ -121,7 +121,7 @@ public final class R {
         public static final int notification_template_part_time = 0x7f0a0059;
     }
     public static final class string {
-        public static final int status_bar_notification_info_overflow = 0x7f0e00a7;
+        public static final int status_bar_notification_info_overflow = 0x7f0e00a8;
     }
     public static final class style {
         public static final int TextAppearance_Compat_Notification = 0x7f0f0128;
--- app/build/generated/source/r/debug/com/google/android/gms/R.java
+++ app/build/generated/source/r/debug/com/google/android/gms/R.java
@@ -62,14 +62,14 @@ public final class R {
         public static final int email = 0x7f080066;
         public static final int icon_only = 0x7f080081;
         public static final int light = 0x7f08008c;
-        public static final int none = 0x7f0800a1;
-        public static final int normal = 0x7f0800a2;
-        public static final int radio = 0x7f0800b9;
-        public static final int standard = 0x7f0800e5;
-        public static final int text = 0x7f0800ed;
-        public static final int text2 = 0x7f0800ee;
-        public static final int wide = 0x7f08010d;
-        public static final int wrap_content = 0x7f080110;
+        public static final int none = 0x7f0800a2;
+        public static final int normal = 0x7f0800a3;
+        public static final int radio = 0x7f0800ba;
+        public static final int standard = 0x7f0800e6;
+        public static final int text = 0x7f0800ee;
+        public static final int text2 = 0x7f0800ef;
+        public static final int wide = 0x7f08010e;
+        public static final int wrap_content = 0x7f080111;
     }
     public static final class integer {
         public static final int google_play_services_version = 0x7f090008;

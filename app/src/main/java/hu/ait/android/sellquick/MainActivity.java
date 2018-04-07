0d45850b70b485b7154d83f8fa2d87ac7aacec97
meganzhao <zhaom@carleton.edu>
2018-04-06 22:14:32 -0500

    created navigation view for mainactivity

--- app/src/main/java/hu/ait/android/sellquick/MainActivity.java
+++ app/src/main/java/hu/ait/android/sellquick/MainActivity.java
@@ -1,13 +1,63 @@
 package hu.ait.android.sellquick;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this,drawerLayout,R.string.drawer_open,R.string.drawer_close);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        drawerLayout.setDrawerListener(toggle);
+        toggle.syncState();
+
+        //FloatingActionButton fab =
+
+        createNavigation();
+    }
+
+    private void createNavigation() {
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        menuItem.setChecked(true);
+                        switch (menuItem.getItemId()) {
+                            case R.id.nav_clothes:
+                                //createAlertDialog();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                            case R.id.nav_books:
+                                //openMap();
+                                drawerLayout.closeDrawer(GravityCompat.START);
+                                break;
+                        }
+                        return false;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(toggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

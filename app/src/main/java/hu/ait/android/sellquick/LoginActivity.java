8aab163c1f3319375657aad4a9061162ec8eb56e
meganzhao <zhaom@carleton.edu>
2018-04-07 13:21:11 -0500

    added an empty login activity page that links to the main page

---
+++ app/src/main/java/hu/ait/android/sellquick/LoginActivity.java
@@ -0,0 +1,94 @@
+package hu.ait.android.sellquick;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth mAuth;
+    private Button button;
+
+    // Choose authentication providers
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+            new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        button = findViewById(R.id.buttonSignin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+            }
+        });
+
+        for (String provider: AuthUI.SUPPORTED_PROVIDERS){
+            Log.v(this.getClass().getName(), provider);
+        }
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void signIn(View view){
+        startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setIsSmartLockEnabled(false)
+                    .setAvailableProviders(providers)
+                    .setTosUrl("https://superapp.example.com/terms-of-service.html")
+                    .setPrivacyPolicyUrl("https://superapp.example.com/privacy-policy.html")
+                    .build(),
+                RC_SIGN_IN
+        );
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                finish();
+            } else {
+                // Sign in failed
+                if (response == null) {
+                    // User pressed back button
+                    //showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    //showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                //showSnackbar(R.string.unknown_error);
+                //Log.e(TAG, "Sign-in error: ", response.getError());
+            }
+        }
+    }
+}
--- app/src/main/java/hu/ait/android/sellquick/MainActivity.java
+++ app/src/main/java/hu/ait/android/sellquick/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import com.facebook.FacebookSdk;
 
 public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;

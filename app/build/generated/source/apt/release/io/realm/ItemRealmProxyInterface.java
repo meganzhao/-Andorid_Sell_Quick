38bdf05bb0d770f3cab8869cd551d8880197fb2e
meganzhao <zhaom@carleton.edu>
2018-04-07 21:31:53 -0500

    fixed bug of gradle sync

---
+++ app/build/generated/source/apt/release/io/realm/ItemRealmProxyInterface.java
@@ -0,0 +1,13 @@
+package io.realm;
+
+
+public interface ItemRealmProxyInterface {
+    public String realmGet$itemID();
+    public void realmSet$itemID(String value);
+    public String realmGet$itemName();
+    public void realmSet$itemName(String value);
+    public float realmGet$price();
+    public void realmSet$price(float value);
+    public int realmGet$countDown();
+    public void realmSet$countDown(int value);
+}
